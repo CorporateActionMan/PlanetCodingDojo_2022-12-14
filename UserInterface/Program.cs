a49157508a5dec021ca82c88e8ffb66f4f1dfd59
Ashley Wagstaffe <ashley.wagstaffe@hoistgroup.com>
2022-12-14 15:33:38 +0000

    main method

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -3,30 +3,38 @@
 using System.Collections.Concurrent;
 using UserInterface;
 
-Console.WriteLine("Welcome to Santa's Delivery Pipeline!");
-
-ToyMachine machine = new ToyMachine();
-Sleigh sleigh = new Sleigh();
-
-ConcurrentQueue<Present> toyStore = new ConcurrentQueue<Present>();
-Parallel.For(0, 10, index =>
+public class Program
 {
-    var present = machine.Generate();
-
-    toyStore.Enqueue(present);
-});
-
-Elf elf = new Elf(sleigh);
-
-Parallel.For(0, toyStore.Count, (presentIndex) =>
-{
-    var didDequeue = toyStore.TryDequeue(out Present? present);
-    if (present != null)
+    public static void Main(String[] args)
     {
-        Console.WriteLine(elf.ReceivePresent(present));
+        Console.WriteLine("Welcome to Santa's Delivery Pipeline!");
+
+        ToyMachine machine = new ToyMachine();
+        Sleigh sleigh = new Sleigh();
+
+        ConcurrentQueue<Present> toyStore = new ConcurrentQueue<Present>();
+        Parallel.For(0, 10, index =>
+        {
+            var present = machine.Generate();
+
+            toyStore.Enqueue(present);
+        });
+
+        Elf elf = new Elf(sleigh);
+
+        Parallel.For(0, toyStore.Count, (presentIndex) =>
+        {
+            var didDequeue = toyStore.TryDequeue(out Present? present);
+            if (present != null)
+            {
+                Console.WriteLine(elf.ReceivePresent(present));
+            }
+        });
+
+        Console.WriteLine($"Done! Toystore Count: { toyStore.Count } Sleigh Count: { sleigh.Presents.Count }");
+
+        Console.ReadLine();
     }
-});
+}
 
-Console.WriteLine($"Done! Toystore Count: { toyStore.Count } Sleigh Count: { sleigh.Presents.Count }");
 
-Console.ReadLine();
